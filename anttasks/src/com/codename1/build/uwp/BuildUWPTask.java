77eb5b2f29979e4cf0c794534f7f2e84668a267d
Steve Hannah <Steve Hannah>
2016-04-01 16:08:49 -0700

    Added placeholder for ant task

---
+++ anttasks/src/com/codename1/build/uwp/BuildUWPTask.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.codename1.build.uwp;
+
+import org.apache.tools.ant.BuildException;
+import org.apache.tools.ant.Task;
+
+/**
+ * Task to build a UWP application.
+ * @author steve
+ */
+public class BuildUWPTask extends Task {
+
+    @Override
+    public void execute() throws BuildException {
+        super.execute(); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
